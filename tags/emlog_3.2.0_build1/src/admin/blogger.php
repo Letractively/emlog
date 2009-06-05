3936df02b7962b3f4754213f61ec8ea570b16ddf
emloog <emloog@5f219224-4642-0410-9343-2d18db063b08>
2009-06-05 00:01:34 +0000

    emlog3.2.0预发布包

---
+++ tags/emlog_3.2.0_build1/src/admin/blogger.php
@@ -0,0 +1,132 @@
+<?php
+/**
+ * 个人资料
+ * @copyright (c) Emlog All Rights Reserved
+ * @version emlog-3.2.0
+ * $Id$
+ */
+
+require_once('globals.php');
+require_once(EMLOG_ROOT.'/model/C_user.php');
+
+if($action == '')
+{
+	$emUser = new emUser($DB);
+	$row = $emUser->getOneUser(UID);
+	extract($row);
+	$icon = '';
+	if ($photo && file_exists($photo))
+	{
+		$imgsize = chImageSize($photo,ICON_MAX_W,ICON_MAX_H);
+		$icon = "<img src=\"{$photo}\" width=\"{$imgsize['w']}\" height=\"{$imgsize['h']}\" border=\"1\" /><a href=\"javascript: em_confirm(0, 'avatar');\">[删除头像]</a>";
+	}
+	include getViews('header');
+	require_once(getViews('blogger'));
+	include getViews('footer');
+	cleanPage();
+}
+
+if($action == 'update')
+{
+	$emUser = new emUser($DB);
+	
+	$flg = isset($_GET['flg']) ? intval($_GET['flg']) : 0;//前台调用标识
+	if(!$flg)
+	{
+		$photo = isset($_POST['photo']) ? addslashes(trim($_POST['photo'])) : '';
+		$nickname = isset($_POST['name']) ? addslashes(trim($_POST['name'])) : '';
+		$email = isset($_POST['email']) ? addslashes(trim($_POST['email'])) : '';
+		$description = isset($_POST['description']) ? addslashes(trim($_POST['description'])) : '';
+	
+		$photo_type = array('gif', 'jpg', 'jpeg','png');
+		if($_FILES['photo']['size'] > 0)
+		{
+			$usericon = uploadFile($_FILES['photo']['name'], $_FILES['photo']['error'], $_FILES['photo']['tmp_name'], $_FILES['photo']['size'], $_FILES['photo']['type'], $photo_type, 1);
+		}else{
+			$usericon = $photo;
+		}
+		
+		$emUser->updateUser(array('nickname'=>$nickname, 'email'=>$email, 'photo'=>$usericon, 'description'=>$description), UID);
+
+		$CACHE->mc_user();
+
+		header("Location: ./blogger.php?active_edit=true");
+	}else {
+		$description = isset($_POST['bdes']) ? addslashes(trim($_POST['bdes'])) : '';
+		$emUser->updateUser(array('description'=>$description), UID);
+		$CACHE->mc_user();
+		echo $description;
+	}
+}
+
+if($action == 'delicon')
+{
+	$query = $DB->query("select photo from ".DB_PREFIX."user");
+	$icon = $DB->fetch_array($query);
+	if(file_exists($icon['photo']))
+	{
+		$fpath = str_replace('thum-', '', $icon['photo']);
+		if($fpath != $icon['photo'])
+		{
+			$ret = unlink($fpath);
+			if(!$ret)
+			{
+				formMsg('头像删除失败','./blogger.php',0);
+			}
+		}
+		$ret = unlink($icon['photo']);
+		if(!$ret)
+		{
+			formMsg('头像删除失败','./blogger.php',0);
+		}
+	}
+	$DB->query("UPDATE ".DB_PREFIX."user SET photo='' ");
+	$CACHE->mc_user();
+	header("Location: ./blogger.php?active_del=true");
+}
+
+if($action == 'update_pwd')
+{
+	require_once(EMLOG_ROOT.'/lib/C_phpass.php');
+	
+	$emUser = new emUser($DB);
+	
+	$login = isset($_POST['username']) ? addslashes(trim($_POST['username'])) : '';
+	$newpass = isset($_POST['newpass']) ? addslashes(trim($_POST['newpass'])) : '';
+	$oldpass = isset($_POST['oldpass']) ? addslashes(trim($_POST['oldpass'])) : '';
+	$repeatpass = isset($_POST['repeatpass']) ? addslashes(trim($_POST['repeatpass'])) : '';
+	
+	$PHPASS = new PasswordHash(8, true);
+	$ispass = checkPassword($oldpass, $userData['password']);
+
+	if(!$ispass)
+	{
+		formMsg('错误的当前密码','javascript:history.go(-1);',0);
+	}elseif(!empty($login) && $emUser->isUserExist($login, UID)){
+		formMsg('用户名已存在','javascript:history.go(-1);',0);
+	}elseif(strlen($newpass)>0 && strlen($newpass) < 6){
+		formMsg('密码长度不得小于6位','javascript:history.go(-1);',0);
+	}elseif(!empty($newpass) && $newpass != $repeatpass){
+		formMsg('两次输入的密码不一致','javascript:history.go(-1);',0);
+	}
+
+	if(!empty($newpass) && empty($login))//只修改密码
+	{
+		$newpass = $PHPASS->HashPassword($newpass);
+		$emUser->updateUser(array('password'=>$newpass), UID);
+		formMsg('密码修改成功!','./',1);
+	}elseif(!empty($newpass) && !empty($login))//修改密码及用户
+	{
+		$newpass = $PHPASS->HashPassword($newpass);
+		$emUser->updateUser(array('username'=>$login, 'password'=>$newpass), UID);
+		formMsg('密码和后台登录名修改成功!请重新登录','./',1);
+	}elseif(empty($newpass) && !empty($login))//只修改后台登录名
+	{
+		$emUser->updateUser(array('username'=>$login), UID);
+		formMsg('后台登录名修改成功!请重新登录','./',1);
+	}else{
+		formMsg('请输入要修改的项目','javascript:history.go(-1);',0);
+	}
+}
+
+?>
---
+++ tags/emlog_3.2.0_build1/src/admin/globals.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * 后台全局项加载
+ * @copyright (c) Emlog All Rights Reserved
+ * @version emlog-3.2.0
+ * $Id$
+ */
+
+require_once('../init.php');
+
+//高级配置选项
+define('UPLOADFILE_MAXSIZE', 20971520);//附件大小上限 单位：字节（默认20M）
+define('UPLOADFILE_PATH', '../content/uploadfile/');//附件保存目录
+define('IS_THUMBNAIL', 0);//上传图片是否生成缩略图 1:是 0:否
+define('ADMIN_PERPAGE_NUM', 15);//后台管理每页条目数
+define('ADMIN_TPL', 'default');//后台模板名
+define('ADMIN_ROOT', dirname(__FILE__));
+$att_type = array('rar','zip','gif', 'jpg', 'jpeg', 'png', 'bmp');//允许上传的文件类型
+
+//后台当前模板路径
+$cerTemplatePath = ADMIN_ROOT.'/views/'.ADMIN_TPL.'/';
+if (!is_dir($cerTemplatePath))
+{
+	exit('The Management Center Template Path Error');
+}
+//读取缓存
+$sta_cache = $CACHE->readCache('sta');
+$sort_cache = $CACHE->readCache('sort');
+$user_cache = $CACHE->readCache('user');
+$log_cache_tags = $CACHE->readCache('log_tags');
+
+//登陆验证
+if ($action == 'login')
+{
+	$username = isset($_POST['user']) ? addslashes(trim($_POST['user'])) : '';
+	$password = isset($_POST['pw']) ? addslashes(trim($_POST['pw'])) : '';
+	$ispersis = isset($_POST['ispersis']) ? intval($_POST['ispersis']) : false;
+	$img_code = ($login_code == 'y' && isset($_POST['imgcode'])) ? addslashes(trim(strtoupper($_POST['imgcode']))) : '';
+
+	if (checkUser($username, $password, $img_code, $login_code) === true)
+	{
+		setAuthCookie($username, $ispersis);
+		header("Location: ../");
+	}else{
+		loginPage();
+	}
+}
+//登出
+if ($action == 'logout')
+{
+	session_start();
+	session_unset();
+	session_destroy();
+	setcookie(AUTH_COOKIE_NAME, ' ', time() - 31536000, '/');
+	formMsg('退出成功！','../',1);
+}
+
+if(ISLOGIN === false)
+{
+	loginpage();
+}
+
+$request_uri = strtolower(substr(basename($_SERVER['SCRIPT_NAME']), 0, -4));
+if (ROLE == 'writer' && !in_array($request_uri, array('write_log','admin_log','attachment','blogger','comment','index','save_log','trackback')))
+{
+	formMsg('权限不足！','./', 0);
+}
+
+?>
